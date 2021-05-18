6da912d1e80c4df8c9bf01b7b5a1175078744ed0
Eduardo Brito <alu0100783315@ull.edu.es>
2021-05-18 17:18:03 +0100

    findOne working

---
+++ src/findOne.ts
@@ -0,0 +1,25 @@
+import {MongoClient, ObjectID} from 'mongodb';
+
+const dbURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'notes-app';
+
+interface NoteInterface {
+  title: string,
+  body: string,
+  color: 'blue' | 'green' | 'red' | 'yellow' | 'magenta'
+}
+
+MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<NoteInterface>('notes').findOne({
+    _id: new ObjectID('60a3e6eaea46fc1b72fa2641'),
+  });
+}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
